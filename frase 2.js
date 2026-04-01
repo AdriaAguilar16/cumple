d2ad3863f4928d6744f936e791835f31a2d64536
Adrian Alejandro Aguilar Anchundia <adriana@tecnologicoismac.edu.ec>
2026-04-01 00:02:37 -0500

    Adjust timeout values for letter display timing
    Increased timeout durations for letter display and visibility.

--- frase 2.js
+++ frase 2.js
@@ -24,19 +24,19 @@ function consoleText(words, id, colors) {
         target.setAttribute('style', 'color:' + colors[0])
         letterCount += x;
         waiting = false;
-      }, 400)
+      }, 1000)
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
       window.setTimeout(function() {
         x = -1;
         letterCount += x;
         waiting = false;
-      }, 400)
+      }, 1000)
     } else if (waiting === false) {
       target.innerHTML = words[0].substring(0, letterCount)
       letterCount += x;
     }
-  }, 10)
+  }, 60)
   window.setInterval(function() {
     if (visible === true) {
       con.className = 'console-underscore hidden'
@@ -47,5 +47,5 @@ function consoleText(words, id, colors) {
 
       visible = true;
     }
-  }, 400)
+  }, 600)
 }
